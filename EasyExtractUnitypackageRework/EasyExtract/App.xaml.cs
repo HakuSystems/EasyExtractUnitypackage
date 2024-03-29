2520337ecbe614594404967b36a7cbc94c80feab
lyze <66133638+HakuSystems@users.noreply.github.com>
2024-03-29 14:24:37 +0100

    Update code comments and project paths
    Updated the auto-generated comment translations from German to English in several files. Also adjusted some project paths in the nuget.dgspec.json file. Additionally, the commit includes changes in project settings via cleaning up the .csproj & .sln files.

---
+++ EasyExtractUnitypackageRework/EasyExtract/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace EasyExtract;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+    {
+        MessageBox.Show(e.Exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        e.Handled = true;
+    }
+}
