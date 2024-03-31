ad0c1ede7ffcf096d69480685245c4b49cd322c8
lyze <66133638+HakuSystems@users.noreply.github.com>
2024-04-01 00:02:58 +0200

    Update UI elements and improve code structure
    This commit makes changes to various UI elements in the "About" page and the dashboard, enhancing user experience. It further refines the structure and properties of the "About" code for better maintainability. Additionally, the Assembly version is updated within the project configuration.

--- EasyExtractUnitypackageRework/EasyExtract/UserControls/About.xaml.cs
+++ EasyExtractUnitypackageRework/EasyExtract/UserControls/About.xaml.cs
@@ -1,6 +1,5 @@
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 using System.Windows.Threading;
 using Wpf.Ui.Controls;
 
@@ -8,7 +7,8 @@ namespace EasyExtract.UserControls;
 
 public partial class About : UserControl
 {
-    private List<Card> Cards = new();
+    private readonly List<Card> _cards = [];
+
     public About()
     {
         InitializeComponent();
@@ -16,7 +16,9 @@ public partial class About : UserControl
 
     private void About_OnLoaded(object sender, RoutedEventArgs e)
     {
-        var maxCards = 10;
+        VersionCard.Footer = $"Version {Application.ResourceAssembly.GetName().Version}";
+
+        const int maxCards = 10;
         for (var i = 0; i < maxCards; i++)
         {
             var card = new Card
@@ -24,28 +26,24 @@ public partial class About : UserControl
                 Height = 20,
                 HorizontalAlignment = HorizontalAlignment.Stretch,
                 Padding = new Thickness(5),
-                Margin= RandomMargin()
+                Margin = RandomMargin()
             };
-            Cards.Add(card);
+            _cards.Add(card);
             RandomCardDesign.Items.Add(card);
         }
-        var repeatTimer = new DispatcherTimer {Interval = TimeSpan.FromMilliseconds(500)};
+        var repeatTimer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(500) };
         repeatTimer.Tick += (o, args) => ChangeRandomMargins();
         repeatTimer.Start();
     }
-    
+
     private Thickness RandomMargin()
     {
         var random = new Random();
         return new Thickness(random.Next(0, 20), random.Next(0), random.Next(0, 20), random.Next(0));
     }
-    
+
     private void ChangeRandomMargins()
     {
-        foreach (var card in Cards)
-        {
-            card.Margin = RandomMargin();
-        }
+        foreach (var card in _cards) card.Margin = RandomMargin();
     }
-
 }
